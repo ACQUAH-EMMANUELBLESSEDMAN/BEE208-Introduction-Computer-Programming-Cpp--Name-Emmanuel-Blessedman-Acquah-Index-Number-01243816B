caae24c6c2f1ed5e9fd1a0967ea41ba3be7bf654
ACQUAH-EMMANUELBLESSEDMAN <acquahemmanuelblessedman9@gmail.com>
2026-06-18 10:37:37 +0000

    Add files via upload

---
+++ Question 10.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;
+
+
+int main() {  
+   int y, z;
+    cout << "Enter two integers: ";  
+	cin >> x >> z;
+    int larger = (x > z) ? x : z;  
+	cout << "Larger value: " << larger << endl;
+	
+	
+    return 0;
+}
