fb507884ce0bc39b08824cb8e3c1606078aa1513
ACQUAH-EMMANUELBLESSEDMAN <acquahemmanuelblessedman9@gmail.com>
2026-06-18 09:22:56 +0000

    Add files via upload

---
+++ Question 1.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int m, n;
+	cout<<"Enter two integers:";
+	cin>>m>>n;
+	
+	cout<< "sum:"<<m+n<<endl;
+	cout<<"difference:"<<m-n<<endl;
+	cout<<"product:"<<m*n<<endl;
+	if (n!= 0)
+	cout<<"quotient:"<<m/n<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	else
+	cout<<"quotient:undfined (divide by zero)"<<endl;
+	
+	return 0;
+}
